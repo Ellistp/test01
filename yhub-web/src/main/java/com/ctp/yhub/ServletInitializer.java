14d66c6595eb9a38631382d280c988d7ad3d1e40
1653493637@qq.com <ping2080305>
2018-05-20 21:28:47 +0800

    spring boot 项目第一次提交

---
+++ yhub-web/src/main/java/com/ctp/yhub/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ctp.yhub;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(YhubWebApplication.class);
+	}
+
+}
---
+++ yhub-web/src/test/java/com/ctp/yhub/YhubWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ctp.yhub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YhubWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
