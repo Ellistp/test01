bab27e52af8ec78ab7541534c056f2566d35b555
1653493637@qq.com <ping2080305>
2018-05-27 20:36:40 +0800

    junit4

---
+++ yhub-web/src/test/java/UserControllerTest.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2018/5/27 0027.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MockServletContext.class)
+public class UserControllerTest {
+}
