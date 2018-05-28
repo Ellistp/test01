35d1456e0dc8c992c6f871e9a02457125979eac9
1653493637@qq.com <ping2080305>
2018-05-28 21:03:46 +0800

    junit4

--- yhub-web/src/main/java/com/ctp/yhub/web/WebApplication.java
+++ yhub-web/src/main/java/com/ctp/yhub/web/WebApplication.java
@@ -3,6 +3,11 @@ package com.ctp.yhub.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* 组合了 @Configuration @EnableAutoConfiguration @ComponentScan
+* @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+* 关闭特定的自动配置
+* */
 @SpringBootApplication
 public class WebApplication {
 	public static void main(String[] args) {
