c7bc51f9f720f9d31fd215230f395f4dfc05ff05
wb-ctp278982 <wb-ctp278982@alibaba-inc.com>
2018-08-09 17:20:24 +0800

    update

--- yhub-web/src/main/java/com/yhub/web/config/DefaultView.java
+++ yhub-web/src/main/java/com/yhub/web/config/DefaultView.java
@@ -2,6 +2,7 @@ package com.yhub.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,6 +13,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class DefaultView extends WebMvcConfigurerAdapter {
+
+    //@Bean
+    //public ViewResolver getViewResolver() {
+    //    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+    //    resolver.setPrefix("/WEB-INF/views/");
+    //    resolver.setSuffix(".jsp");
+    //    return resolver;
+    //}
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
